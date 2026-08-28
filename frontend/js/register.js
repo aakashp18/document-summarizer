ae64da50b4baff1c664fe9394d2c004e48f2d933
Aakash Prajapati <your-aakashhp25@outlook.com>
2026-08-28 16:35:14 +0530

    Fix API URLs and frontend routes

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -37,7 +37,7 @@ registerBtn.addEventListener("click", async function () {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:5000/register",
+            "/register",
             {
                 method: "POST",
 
